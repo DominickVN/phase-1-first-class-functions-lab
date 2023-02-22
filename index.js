ce5d4b9deba3b35c240326a28fa4276581af12a0
dominick <vannusdominick@gmail.com>
2023-02-22 08:59:22 -0800

    allTestPassing

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const  returnFirstTwoDrivers = function arrayFunction([]){
+    return (['Antonia', 'Nuru']);
+    }
+
+const  returnLastTwoDrivers = function arrayFunction([]){
+    return (['Amari', 'Mo']);
+    }
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(anInteger){
+    return function(fare){
+      return fare*anInteger
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function  (arrayOfDrivers, driversToReturn) {
+    return driversToReturn(arrayOfDrivers)
+  }
